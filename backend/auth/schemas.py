5465278f6d266634f7bc2c138ba5c07cc03d7c48
Batyr Atayev <atayev2012@gmail.com>
2025-06-12 22:50:37 +0300

    Added schemes for sign up and login

---
+++ backend/auth/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, Field, EmailStr
+from datetime import date
+
+class UserSignUp(BaseModel):
+    username: str = Field(max_length=50, description="unique username", examples=["nerd1995"])
+    email: EmailStr = Field(max_length=100, description="user email address", examples=["test@mail.ru"])
+    phone: int = Field(description="phone number with country code", examples="79999999999")
+    first_name: str = Field(max_length=50, description="first name", examples=["George"])
+    last_name: str = Field(max_length=50, description="last name", examples=["Washington"])
+    middle_name: str | None = Field(max_length=50, description="optional - middle name")
+    date_of_birth: date = Field(description="date of birth in format YYYY-MM-DD", examples=["2000-02-18"])
+    gender: bool = Field(description="gender - True = male; False = female", examples=[True])
+    password: str = Field(max_length=255, description="password", examples=["password"])
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
