2d919bef0091d663b335e3192931603e12b6d326
Batyr Atayev <atayev2012@gmail.com>
2025-06-14 23:08:08 +0300

    Added email_verify endpoint

--- backend/auth/router.py
+++ backend/auth/router.py
@@ -1,18 +1,20 @@
-from fastapi import APIRouter, Depends, Response, Request, HTTPException, status
+from fastapi import APIRouter, Depends, Response, Request, HTTPException, status, BackgroundTasks
 from auth.schemas import UserLogin, UserSignUp
 from models import User
 from database import SessionDep
 from sqlalchemy import select
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 from auth.utils import (verify_password, create_token, get_password_hash, 
-                        get_user_by_username, set_token_to_cookies)
+                        get_user_by_username, set_token_to_cookies,
+                        send_email_verification_token, get_username)
+from config import config
 
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 # create new user endpoint
 @router.post("/sign_up", status_code=status.HTTP_201_CREATED)
-async def sign_up(user_data: UserSignUp, response: Response, session: SessionDep):  
+async def sign_up(user_data: UserSignUp, response: Response, session: SessionDep, background_tasks: BackgroundTasks):  
     user_data_dict = user_data.model_dump()
     user_data_dict.setdefault("password_hash", get_password_hash(user_data_dict["password"]))
     user_data_dict.pop("password")
@@ -46,10 +48,16 @@ async def sign_up(user_data: UserSignUp, response: Response, session: SessionDep
     return_value = {
         "detail":{
             "success": True,
-            "message": "New user was successfully created",
-            "data": user
+            "message": "New user was successfully created. Email verification link was sent to your address.",
+            "data": {
+                "username": user.username,
+                "email": user.email
+            }
         }
     }
+
+    # send email in background
+    background_tasks.add_task(send_email_verification_token, user)
 
     return return_value
 
@@ -136,3 +144,39 @@ async def active_user(user = Depends(get_user_by_username)):
             "data": user_data
         }
     }
+
+@router.get("/email_verify/{verify_token}")
+async def email_verify(verify_token, session: SessionDep):
+    username = await get_username(verify_token, secret_key=config.EMAIL_VERIFICATION_SECRET_KEY)
+    user = await get_user_by_username(session=session, username=username)
+    if user.email_verified:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={
+                "success": False,
+                "message": "Email was already verified.",
+                "data": None
+            }
+        )
+    
+    user.email_verified = True
+
+    try:
+        await session.commit()
+    except SQLAlchemyError:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail={
+                "success": False,
+                "message": "Server internal error occured. Try again later.",
+                "data": None
+            }
+        )
+    
+    return_value = {
+        "success": True,
+        "message": f"Email {user.email} was successfully verified.",
+        "data": None
+    }
+
+    return return_value
