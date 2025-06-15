bdfa7d3048cc94a3f044248bcf4f618f0be52996
Batyr Atayev <atayev2012@gmail.com>
2025-06-15 16:02:20 +0300

    Added guest_router and user_router

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from auth.router import router as auth_router
+from guest.router import router as guest_router
+from user.router import router as user_router
 
 app = FastAPI()
 app.include_router(auth_router, prefix="/api")
+app.include_router(guest_router, prefix="/api")
+app.include_router(user_router, prefix="/api")
 
 
 # testing app
